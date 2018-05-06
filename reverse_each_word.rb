726f5676d7373c37870ce01133bf27f9aa9d2e49
Anthony Lam <al3101@nyu.edu>
2018-05-06 18:33:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(string)
   array = string.split
-  reversed = []
   array.collect do |word| 
     word = word.reverse 
-    reversed.push(word) 
   end
-  return reversed.join(" ")
 end 
     
