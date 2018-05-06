9ba7a37ed4b261effbc4344594d1f2b848ef38e9
Anthony Lam <al3101@nyu.edu>
2018-05-06 18:29:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,10 @@
-reverse_each_word(string)
+def reverse_each_word(string)
+  array = string.split
+  reversed = ""
+  array.each do |word| 
+    word = word.reversed 
+    reversed.push(word) 
+  end
+  return reversed
+end 
+    
